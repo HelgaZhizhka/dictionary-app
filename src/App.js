b2f307cc815af0a9123d272eff5a95a3a4b29db2
Olga Zhyzhka <helgajijika@gmail.com>
2022-09-07 19:43:37 +0300

    add
    add first changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="App-logo" />
+        <h1>Hello React</h1>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
