7d725fbceae60e2b98e8a5c60642dc4906f2f1f5
Olga Zhyzhka <helgajijika@gmail.com>
2022-09-07 22:13:44 +0300

    add axios and dictionary api
    add dictionary api only for en words

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,20 @@
-import React, {useState} from "react";
+import React, {useState} from "react"   
+import axios from "axios"
 import "./search.css"
+
 
 export default function Search(props) {
     let [searchValue, setSearchValue] = useState(null)
+
+    function handleResponse(response) {
+		console.log(response.data[0]);
+	}
+
     function search(event) {
         event.preventDefault();
         alert(`Searching for ${searchValue}`);
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`;
+        axios.get(apiUrl).then(handleResponse).catch(error => console.log(error))
     }
     function handlerChange(event) {
 		setSearchValue(event.target.value)
@@ -14,11 +23,13 @@ export default function Search(props) {
     return (
 		<div className="search">
 			<form onSubmit={search}>
-				<input
-					className="search__input"
-					type="search"
-					onChange={handlerChange}
-				/>
+				<div className="d-flex">
+					<input
+						className="search__input"
+						type="search"
+						onChange={handlerChange}
+					/>``
+				</div>
 			</form>
 		</div>
 	);
