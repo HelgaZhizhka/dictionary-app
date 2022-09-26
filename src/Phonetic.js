0a068c13053f935254543f16be6555cf3ced69fb
Olga Zhyzhka <helgajijika@gmail.com>
2022-09-26 16:47:21 +0300

    styleing

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,11 +3,14 @@ import Audio from "./Audio";
 export default function Phonetic(props) {
     if (props) {
         return (
-          <div className="d-flex">
-            {props.phonetic.audio && (
-              <Audio audio={props.phonetic.audio} />
-            )}
-            <span className="search__transcription">{props.phonetic.text}</span>
+          <div className="d-flex mr-2">
+            {props.phonetic.audio && <Audio audio={props.phonetic.audio} />}
+            <span
+              className="search__transcription"
+              style={{ marginLeft: '10px' }}
+            >
+              {props.phonetic.text}
+            </span>
           </div>
         )
     } else {
--- src/SearchData.js
+++ src/SearchData.js
@@ -11,13 +11,11 @@ export default function SearchData(props) {
           <h2 className="title">{`${props.data.data.word[0].toUpperCase()}${props.data.data.word.slice(
             1
           )}`}</h2>
-          {props.data.data.phonetics.map((phonetic, index) => {
-            return (
-              <div className="search__pnonetics" key={index}>
-                <Phonetic phonetic={phonetic} />
-              </div>
-            )
-          })}
+          <div className="d-flex mr-2">
+            {props.data.data.phonetics.map((phonetic, index) => {
+              return <Phonetic phonetic={phonetic} key={index} />
+            })}
+          </div>
         </div>
         {props.data.data.meanings.map((meaning, index) => {
           return (
