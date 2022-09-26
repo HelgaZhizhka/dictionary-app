9fafeaf0290719de02a894f41280b2e6fac58a9a
Olga Zhyzhka <helgajijika@gmail.com>
2022-09-26 18:31:45 +0300

    add photos

--- src/App.js
+++ src/App.js
@@ -4,14 +4,40 @@ import Search from "./Search";
 
 function App() {
   return (
-		<div className="container">
-			<header className="header">
-				<img className="logo" src={logo} alt="App-logo" />
-				<h1>Dictionary App</h1>
-			</header>
-			<Search word="transition" />
-		</div>
-  );
+    <div className="container">
+      <header className="header">
+        <img className="logo" src={logo} alt="App-logo" />
+        <h1>Dictionary App</h1>
+      </header>
+      <Search word="transition" />
+      <footer>
+        This project was coded by{' '}
+        <a
+          href="https://www.shecodes.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SheCodes
+        </a>{' '}
+        and is{' '}
+        <a
+          href="https://github.com/HelgaZhizhka/dictionary-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{' '}
+        and{' '}
+        <a
+          href="https://dictionary-webdev.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  )
 }
 
 export default App;
